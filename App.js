6640a8c77d4d59e93d495faabd9d50c796e2c68a
torranicles <torranicles@gmail.com>
2021-07-03 03:28:02 +0800

    Add firebase and env vars

--- App.js
+++ App.js
@@ -1,16 +1,43 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import LandingPage from './components/Landing'
+import { 
+  API_KEY, 
+  AUTH_DOMAIN, 
+  PROJECT_ID, 
+  STORAGE_BUCKET, 
+  MESSAGING_SENDER_ID, 
+  APP_ID, 
+  MEASUREMENT_ID 
+} from '@env';
+import LandingPage from './components/Landing';
+import RegisterPage from './components/Register';
+import * as firebase from 'firebase';
 
+const firebaseConfig = {
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID
+};
+   
 const Stack = createStackNavigator();
+
+if (firebase.apps.length === 0) { //Ensure no other firebase instance is running
+  firebase.initializeApp(firebaseConfig);
+};
+
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Landing">
         <Stack.Screen name="Landing" component={LandingPage} options={{ headerShown: false }} />
+        <Stack.Screen name="Register" component={RegisterPage} />
       </Stack.Navigator>
     </NavigationContainer>
   );

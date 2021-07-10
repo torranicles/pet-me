e9e78c6bb599da89959e8f6bae03b18978bdbf93
torranicles <torranicles@gmail.com>
2021-07-10 18:24:20 +0800

    Update camera ('add') component

--- components/main/Add.js
+++ components/main/Add.js
@@ -1,12 +1,55 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, Button} from 'react-native';
+import { Camera } from 'expo-camera';
+
 
 function Add(props) {
+    const [hasPermission, setHasPermission] = useState(null);
+    const [type, setType] = useState(Camera.Constants.Type.back);
+
+    useEffect(() => {
+        (async () => {
+            const { status } = await Camera.requestPermissionsAsync();
+            setHasPermission(status === 'granted');
+        })();
+
+    }, []);
+
+    const flipCam = () => {
+        setType(
+            type === Camera.Constants.Type.back
+            ? Camera.Constants.Type.front
+            : Camera.Constants.Type.back
+        )
+    }
     return (
-        <View>
-            <Text>Add</Text>
-        </View>
-    );
+        hasPermission === null //Error
+        ?   <View/>
+        : hasPermission === false
+        ?   <Text>
+                App does not have permission to access the camera.
+            </Text>
+        :   <View style={styles.container}>
+                <View style={styles.camContainer}>
+                    <Camera style={styles.fixedRatio} type={type} ratio={'1:1'}/>
+                </View>
+                <Button
+                    title="Flip Camera"
+                    onPress={flipCam}/>
+            </View>
+    )
 }
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    camContainer: {
+        flexDirection: 'row'
+    },
+    fixedRatio: {
+        flex: 1,
+        aspectRatio: 1
+    }
+})
 export default Add;

3eb0add35965a66446296593fa50ed2d69ee161e
torranicles <torranicles@gmail.com>
2021-07-25 11:29:36 +0800

    Update Feed

--- components/main/Feed.js
+++ components/main/Feed.js
@@ -1,12 +1,52 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, View, Text, Image, Button, FlatList, StyleSheet, StatusBar, Platform } from 'react-native';
+import { connect } from 'react-redux';
+import firebase from 'firebase';
+import 'firebase/firestore';
 
 function Feed(props) {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        let posts = [];
+        if (props.usersLoaded == props.following.length) { //If all users are loaded
+            for (let i = 0; i < props.following.length; i++) {
+                const user = getState().props.users.find(el => el.uid === props.following[i]);
+            }
+        }
+    }, [props.usersLoaded]);
     return (
-        <View>
-            
-        </View>
-    );
+        <SafeAreaView style={styles.container}>
+            <View style={styles.galleryContainer}>
+            </View>
+        </SafeAreaView>
+    ); 
 }
 
-export default Feed;
+const mapStateToProps = (store) => ({
+    currentUser: store.userState.currentUser,
+    users: store.userState.users,
+    usersLoaded: store.usersState.usersLoaded,
+    following: store.userState.following
+})
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0 
+    },
+    infoContainer: {
+        margin: 15
+    },
+    galleryContainer: {
+        flex: 1
+    },
+    imageContainer: {
+        flex: 1/3   
+    },
+    image: {
+        flex: 1,
+        aspectRatio: 1/1
+    }
+})
+export default connect(mapStateToProps, null)(Feed);

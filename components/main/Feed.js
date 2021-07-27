55f9a4457c6e66ad7315dbc699756d88073458c6
torranicles <torranicles@gmail.com>
2021-07-27 19:34:42 +0800

    Add rendering posts

--- components/main/Feed.js
+++ components/main/Feed.js
@@ -12,12 +12,30 @@ function Feed(props) {
         if (props.usersLoaded == props.following.length) { //If all users are loaded
             for (let i = 0; i < props.following.length; i++) {
                 const user = getState().props.users.find(el => el.uid === props.following[i]);
+                if (user != undefined) {
+                    posts = [...posts, ...user.posts]
+                }
             }
+            posts.sort((x, y) => {
+                return x.creation - y.creation;
+            });
+            setPosts(posts);
         }
     }, [props.usersLoaded]);
     return (
         <SafeAreaView style={styles.container}>
             <View style={styles.galleryContainer}>
+                <FlatList
+                    numColumns={1}
+                    horizontal={false}
+                    data={posts}
+                    renderItem={({ item }) => (
+                        <View style={stles.imageContainer}>
+                            <Text style={styles.container}>{item.user.name}</Text>
+                            <Image style={styles.image} source={{uri: item.downloadUrl}}/>
+                        </View>
+                    )}
+                />
             </View>
         </SafeAreaView>
     ); 
